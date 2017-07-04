cd559c2d4bad879be4cf01296979f3b6cafa921b
TheNickWilson <thenickwilson@gmail.com>
2017-07-04 15:15:28 +0100

    Added a Game Started page and better error display

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -2,21 +2,31 @@ package controllers
 
 import javax.inject._
 
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc._
 import forms.StartGameForm.startGameForm
+import play.api.Logger
 
 import scala.concurrent.Future
 
-class GameController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class GameController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
 
-  def startGame() = Action { implicit request =>
-    Ok(views.html.start_game())
+  def startGame() = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.start_game(startGameForm))
   }
 
-  def startGamePost() = Action.async { implicit request =>
+  def startGamePost() = Action.async { implicit request: MessagesRequest[AnyContent] =>
     startGameForm.bindFromRequest().fold(
-      formWithErrors => Future.successful(BadRequest),
-      gameInfo => Future.successful(Ok)
+      formWithErrors => {
+        Logger.debug(s"* Bad request to startGamePost with this payload: $formWithErrors")
+        Future.successful(BadRequest(views.html.start_game(formWithErrors)))}, {
+          gameInfo =>
+            Logger.debug(s"Request to startGamePost with this payload: $gameInfo")
+            Future.successful(Redirect(routes.GameController.gameStarted))
+      }
     )
+  }
+
+  def gameStarted() = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.game_started())
   }
 }
--- test/controllers/GameControllerSpec.scala
+++ test/controllers/GameControllerSpec.scala
@@ -3,15 +3,17 @@ package controllers
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.libs.json.Json
+import play.api.mvc.AnyContentAsEmpty
 import play.api.test.Helpers.GET
 import play.api.test.{FakeRequest, Injecting}
 import play.api.test.Helpers._
+import play.api.test.CSRFTokenHelper._
 
 class GameControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
-  "GameContoller GET" should {
+  "GameContoller GET to /start-game" should {
 
-    "render the page from the router" in {
+    "render the page" in {
       val request = FakeRequest(GET, "/start-game")
       val result = route(app, request).get
 
@@ -20,28 +22,52 @@ class GameControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       contentAsString(result) must include("Start a new game")
     }
 
-    "respond with 400 to a POST with no payload" in {
-      val request = FakeRequest(POST, "/start-game")
+    "include a form that POSTs to start-game" in {
+      val request = FakeRequest(GET, "/start-game")
       val result = route(app, request).get
+
+      contentAsString(result) must include("""<form action="/start-game" method="POST" >""")
+    }
+  }
+
+  "GameController startGame" should {
+    "respond with 400 to no payload" in {
+      val controller = app.injector.instanceOf[GameController]
+      val request = FakeRequest().withCSRFToken
+      val result = controller.startGamePost().apply(request)
 
       status(result) mustBe BAD_REQUEST
     }
 
-    "respond with 200 to a POST with a valid payload" in {
+    "respond to a valid payload by redirecting to the 'game started' page" in {
       val gameInfo = Map("pointsToWin" -> "1000", "maxRounds" -> "2000", "dynamiteCount" -> "100")
       val request = FakeRequest(POST, "/start-game").withJsonBody(Json.toJson(gameInfo))
       val result = route(app, request).get
 
-      status(result) mustBe OK
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(routes.GameController.gameStarted().url)
     }
 
-    "respond with 400 to a POST with an invalid payload" in {
+    "respond with 400 and an error message to an invalid payload" in {
       val gameInfo = Map("pointsToWin" -> "1000", "maxRounds" -> "2000", "dynamiteCount" -> "abc")
-      val request = FakeRequest(POST, "/start-game").withJsonBody(Json.toJson(gameInfo))
-      val result = route(app, request).get
+      val controller = app.injector.instanceOf[GameController]
+      val request = FakeRequest().withJsonBody(Json.toJson(gameInfo)).withCSRFToken
+      val result = controller.startGamePost().apply(request)
 
       status(result) mustBe BAD_REQUEST
+      contentType(result) mustBe Some("text/html")
+      contentAsString(result) must include("Numeric value expected")
     }
+  }
 
+  "GameController GET to /game-started" should {
+    "render the page" in {
+      val request = FakeRequest(GET, "/game-started")
+      val result = route(app, request).get
+
+      status(result) mustBe OK
+      contentType(result) mustBe Some("text/html")
+      contentAsString(result) must include("Game started!")
+    }
   }
 }
